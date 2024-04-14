3858b1ce638e476058467e32899e1c39ac7a4b89
ganesh-automationtester <ganeshdubbewar21@gmail.com>
2024-04-14 11:39:50 +0530

    driver initialize

--- Test_CICD/conftest.py
+++ Test_CICD/conftest.py
@@ -1,11 +1,27 @@
+import logging
+
+import allure
 import pytest
+from allure_commons.types import AttachmentType
 from selenium import webdriver
-from selenium.webdriver.chrome import service as ChromeService
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(autouse=True)
 def setup(request):
+    op = webdriver.ChromeOptions()
+    op.add_argument('headless')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     print('test start')
+    driver.implicitly_wait(10)
+    driver.delete_all_cookies()
+    driver.maximize_window()
+    driver.get('https://www.google.com')
+    request.cls.driver = driver
+    before_failed = request.session.testsfailed
     yield
+    if request.session.testsfailed != before_failed:
+        allure.attach(driver.get_screenshot_as_png(), name="Test failed", attachment_type=AttachmentType.PNG)
+    driver.quit()
     print('test end')
--- Test_CICD/tests/test_fail_pass.py
+++ Test_CICD/tests/test_fail_pass.py
@@ -1,8 +1,9 @@
 class Test_Allure:
 
     def test_pass(self):
-        assert True
         print('Test passed')
+        assert True
+
 
     def test_fail(self):
         print('Test failed')
